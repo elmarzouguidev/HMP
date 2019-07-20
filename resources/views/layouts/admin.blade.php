541f9202f56f84e5eaa6cde6dbdd74dcc5bfd74b
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2019-07-20 23:43:32 +0000

    configure the layouts of admin panel

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="devscript abdelghafour">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Haymac').' Administration' }}</title>
+
+    <link rel="icon" type="image/png" sizes="16x16" href="../backend/images/favicon.png">
+
+    <link href="{{ asset('backend/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/plugins/chartist-js/dist/chartist.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/plugins/chartist-js/dist/chartist-init.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/plugins/css-chart/css-chart.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/plugins/toast-master/css/jquery.toast.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('backend/colors/blue.css') }}" id="theme" rel="stylesheet">
+    <!-- Styles -->
+
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body  class="fix-header fix-sidebar card-no-border">
+
+<!-- ============================================================== -->
+<!-- Preloader - style you can find in spinners.css -->
+<!-- ============================================================== -->
+<div class="preloader">
+    <svg class="circular" viewBox="25 25 50 50">
+
+        <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+
+    </svg>
+</div>
+<!-- ============================================================== -->
+
+<div id="main-wrapper">
+
+    @include('layouts.navbar.Admin_Top_Nav')
+
+    @include('layouts.navbar.Admin_Left_Nav')
+
+    @include('layouts.navbar.Admin_Right_Nav')
+
+    <div class="page-wrapper">
+
+        <div class="container-fluid">
+
+            @include('layouts.navbar.Admin_Bread_Nav')
+
+        </div>
+
+        @yield('content')
+
+    </div>
+    @include('layouts.footer.Admin_Footer')
+</div>
+<script src="{{ asset('backend/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/bootstrap/js/popper.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/jquery.slimscroll.js') }}"></script>
+<script src="{{ asset('js/waves.js') }}"></script>
+<script src="{{ asset('js/sidebarmenu.js') }}"></script>
+<script src="{{ asset('backend/plugins/sticky-kit-master/dist/sticky-kit.min.js') }}"></script>
+<script src="{{ asset('js/custom.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/chartist-js/dist/chartist.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js') }}"></script>
+<script src="{{ asset('backend/plugins/echarts/echarts-all.js') }}"></script>
+<script src="{{ asset('backend/plugins/toast-master/js/jquery.toast.js') }}"></script>
+<script src="{{ asset('js/dashboard1.js') }}"></script>
+<script src="{{ asset('js/toastr.js') }}"></script>
+
+<script src="{{ asset('backend/plugins/styleswitcher/jQuery.style.switcher.js') }}"></script>
+
+@yield('specified_script')
+</body>
+</html>
---
+++ resources/views/layouts/navbar/Admin_Left_Nav.blade.php
@@ -0,0 +1,95 @@
+<!-- ============================================================== -->
+<!-- Left Sidebar - style you can find in sidebar.scss  -->
+<!-- ============================================================== -->
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar">
+        <!-- User profile -->
+        <div class="user-profile">
+            <!-- User profile image -->
+            <div class="profile-img"> <img src="{{route('admin.articles.file',['filename'=>Auth::user()->avatar])}}" alt="user" /> </div>
+            <!-- User profile text-->
+            <div class="profile-text">
+                <a href="#" class="dropdown-toggle link u-dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">
+                    {{Auth::user()->prenom}} <span class="caret"></span>
+                </a>
+                <div class="dropdown-menu animated flipInY">
+
+                    <a href="{{route('admin.profile')}}" class="dropdown-item"><i class="ti-settings"></i> My Settings</a>
+                    <div class="dropdown-divider"></div> <a href="{{route('admin.logout')}}" class="dropdown-item"><i class="fa fa-power-off"></i> Logout</a>
+                </div>
+            </div>
+        </div>
+        <!-- End User profile text-->
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav">
+                <li class="nav-small-cap">PERSONAL</li>
+                <li>
+                    <a class="has-arrow" href="#" aria-expanded="false"><i class="fa fa-image"></i><span class="hide-menu">Gestion de Galerie </span></a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="{{route('admin.galerie')}}">Modification l’ordre des images</a></li>
+                   
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow " href="#" aria-expanded="false"><i class="mdi mdi-bullseye"></i><span class="hide-menu">Gestion de Société</span></a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="{{route('admin.societes')}}">Ajouter une société </a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow " href="#" aria-expanded="false"><i class="mdi mdi-bullseye"></i><span class="hide-menu">Gestion de projects</span></a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="{{route('admin.projects')}}">Ajouter un project </a></li>
+
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow " href="#" aria-expanded="false"><i class="mdi mdi-bullseye"></i><span class="hide-menu">Gestion des articles</span></a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="{{route('admin.articles')}}">Ajouter un article </a></li>
+
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow " href="#" aria-expanded="false"><i class="fa fa-address-card"></i><span class="hide-menu">Gestion des Prospects</span></a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="{{route('admin.prospects')}}">liste des prospects </a></li>
+               
+                    </ul>
+                </li>
+ 
+                <li>
+                        <a class="has-arrow " href="#" aria-expanded="false"><i class="fa fa-user"></i><span class="hide-menu">Gestion des users</span></a>
+                        <ul aria-expanded="false" class="collapse">
+                            <li><a href="{{route('admin.all')}}">liste des utulisateurs </a></li>
+                            <li><a href="{{route('admin.add')}}">Ajouter un utulisateur </a></li>
+                            <li><a href="{{route('admin.roles')}}">Gestion des permission </a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-devider"></li>
+                   
+                <li class="nav-small-cap">FORMS, TABLE &amp; WIDGETS</li>
+
+
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+    <!-- Bottom points-->
+    <div class="sidebar-footer">
+        <!-- item-->
+        <a href="" class="link" data-toggle="tooltip" title="Settings"><i class="ti-settings"></i></a>
+        <!-- item-->
+        <a href="" class="link" data-toggle="tooltip" title="Email"><i class="mdi mdi-gmail"></i></a>
+        <!-- item-->
+        <a href="" class="link" data-toggle="tooltip" title="Logout"><i class="mdi mdi-power"></i></a>
+    </div>
+    <!-- End Bottom points-->
+</aside>
+<!-- ============================================================== -->
+<!-- End Left Sidebar - style you can find in sidebar.scss  -->
+<!-- ============================================================== -->
