2114c9982b2fa525125b117d35d7676855dc21b7
devscript-abdo <elmarzouguiabdelghafour@gmail.com>
2019-07-20 23:11:43 +0000

    Deploy the new version of site

---
+++ database/migrations/2019_07_20_223319_create_articles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('content');
+            $table->string('file')->nullable();
+            $table->boolean('active')->default(false);
+            $table->integer('category_id')->unsigned();
+            $table->integer('admin_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('admin_id')->references('id')->on('admins');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
---
+++ database/migrations/2019_07_20_225439_create_galleries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('societie_id');
+            $table->unsignedInteger('project_id');
+            $table->string('file');
+            $table->string('type')->nullable();
+            $table->timestamps();
+            $table->foreign('societie_id')->references('id')->on('societies')->onDelete('cascade');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galleries');
+    }
+}
